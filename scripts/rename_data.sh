c62b909d4b374910b768b88d9fee39dc0706d6fa
Sampson Liao <sampsonliao@sliao-macbook-pro.local>
2019-04-01 03:04:05 -0700

    enable job control to speed up renaming

--- scripts/rename_data.sh
+++ scripts/rename_data.sh
@@ -1,6 +1,40 @@
 #!/bin/sh
 
-# Ideally place in the same directory as the train.sh script
+# Place in the same directory as the train.sh script
+
+rename_xml(){
+  # grab filename and extension
+  filepath="${1%.*}"               # data/annotations/STAM123
+  filename="${filepath#$anno_dir}" # STAM123
+  extension="${1##*.}"             # xml
+
+  # construct new numeric filename
+  numname="${filename//[!0-9]/}"
+  numfullpath="${anno_dir}${numname}.${extension}"
+
+  # replace all occurences with updated filename to match xml
+  if [[ "$OSTYPE" == "darwin"* ]]; then
+    sed -i '' "s/${filename}.JPG/${numname}/" $1
+  else
+    sed -i "s/${filename}.JPG/${numname}/" $1
+  fi
+
+  # rename *.xml with numeric filename
+  mv "${1}" "${numfullpath}" 2> /dev/null  # data/annotations/123.xml
+
+  # add to train.txt list
+  echo "${numfullpath}" >> "${anno_dir}train.txt"
+}
+
+rename_jpg(){
+  filepath="${1%.*}"                # data/imgs/STAM123
+  filename="${filepath#$imgs_dir}"  # STAM123
+
+  mv "${1}" "${imgs_dir}${filename//[!0-9]/}" 2> /dev/null
+  printf "\b${sp:i++%${#sp}:1}"
+}
+
+set -m
 
 while getopts a:i: options; do
   case $options in
@@ -28,54 +62,35 @@ echo "images directory = $imgs_dir"
 read -p "Proceed with renaming? (y/n) " -n 1 -r
 echo
 
+
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-  echo -n "Renaming xml files.. "
+  echo "Renaming xml files.. "
+
   # For XML
   anno_dir="${anno_dir%/}/"
 
   rm "${anno_dir}train.txt"
-  i=1
-  sp="/-\|"
+
   for file in ${anno_dir}*.xml
   do
-      # grab filename and extension
-      filepath="${file%.*}"               # data/annotations/STAM123
-      filename="${filepath#$anno_dir}"    # STAM123
-      extension="${file##*.}"             # xml
-
-      # construct new numeric filename
-      numname="${filename//[!0-9]/}"
-      numfullpath="${anno_dir}${numname}.${extension}"
-
-      # replace all occurences with updated filename to match xml
-      if [[ "$OSTYPE" == "darwin"* ]]; then
-        sed -i '' "s/${filename}.JPG/${numname}/" $file
-      else
-        sed -i "s/${filename}.JPG/${numname}/" $file
-      fi
-
-      # rename *.xml with numeric filename
-      mv "${file}" "${numfullpath}" 2> /dev/null  # data/annotations/123.xml
-
-      # add to train.txt list
-      echo "${numfullpath}" >> "${anno_dir}train.txt"
-      printf "\b${sp:i++%${#sp}:1}"
+    rename_xml $file $anno_dir &
   done
+
+  # bring background job to foreground
+  while [ 1 ]; do fg 2> /dev/null; [ $? == 1 ] && break; done
   echo " Done."
 
   # For Images
-  echo -n "Renaming JPG files.. "
+  echo "Renaming JPG files.. "
 
   imgs_dir="${imgs_dir%/}/"
 
   for file in ${imgs_dir}*.JPG
   do
-    filepath="${file%.*}"             # data/imgs/STAM123
-    filename="${filepath#$imgs_dir}"  # STAM123
-
-    mv "${file}" "${imgs_dir}${filename//[!0-9]/}" 2> /dev/null
-    printf "\b${sp:i++%${#sp}:1}"
+    rename_jpg $file $imgs_dir &
   done
+
+  while [ 1 ]; do fg 2> /dev/null; [ $? == 1 ] && break; done
   echo " Done."
 fi
